a1d5dd32c952493745709a596caa6e454954bc45
krishnauppathala67-rgb <krishnauppathala67@gmail.com>
2026-04-24 13:01:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import axios from "axios";
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
